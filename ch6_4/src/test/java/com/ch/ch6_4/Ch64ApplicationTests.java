6c0efc7d18e1f3cc778946a01093b597b14e4669
work202288 <125131806+work202288@users.noreply.github.com>
2023-02-25 19:34:30 +0800

    1

---
+++ ch6_4/src/test/java/com/ch/ch6_4/Ch64ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch.ch6_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch64ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
